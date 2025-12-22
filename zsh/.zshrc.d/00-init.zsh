bd8694018054b589c1659b97c9edabcf36d1eeef
mito99 <mito@live.jp>
2025-12-22 11:09:23 +0900

    add : zsh-completions

--- zsh/.zshrc.d/00-init.zsh
+++ zsh/.zshrc.d/00-init.zsh
@@ -7,3 +7,15 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # mise
 eval "$(mise activate zsh)"
+
+# Homebrewの補完パスを追加
+if type brew &>/dev/null; then
+  # 補完関数の検索パスの先頭に追加
+  fpath=($(brew --prefix)/share/zsh-completions $fpath)
+
+  # 補完機能を有効化 (autoload -U compinit && compinit)
+  # ※ 既に .zshrc 本体のどこかで compinit を呼んでいる場合は、
+  # この fpath 設定をその「前」に書くことが重要です。
+  autoload -U compinit
+  compinit
+fi
